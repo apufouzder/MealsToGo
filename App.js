7bd962ef536ce18ea63987a9112cf75224e1e3ab
apufouzder <apufouzdar@gmail.com>
2021-06-04 01:40:56 +0600

    Checkpoint tab navigation setup

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurant/screens/restaurants.screen";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+
+const Tab = createBottomTabNavigator();
 
 import { useFonts as useOswald, Oswald_400Regular, } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular, } from '@expo-google-fonts/lato';
@@ -19,10 +26,27 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
+  const Map = () => (
+    <SafeArea>
+      <Text>Map...</Text>
+    </SafeArea>
+  );
+  const Settings = () => (
+    <SafeArea>
+      <Text>Settings...</Text>
+    </SafeArea>
+  );
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
